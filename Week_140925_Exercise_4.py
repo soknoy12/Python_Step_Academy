a0875d267c02f6c5b6590b8b8ac1da7181a449ea
CS-Macor <eloonmacor@gmail.com>
2025-09-14 12:23:53 +0700

    Add Week_140925_Exercise_4.py

---
+++ Week_140925_Exercise_4.py
@@ -0,0 +1,23 @@
+
+#write a program that prints the nubers from 1 to 10
+#for count_num in range(0,10):
+    #print(int(count_num)+1)
+
+   
+#write a program that prints the multiplication table of 5 (from 1x5 up to 10x5)
+#mul = int(input("Enter multification number of :"))
+#for index in range(1,11): 
+    #result = mul * index 
+    #print(f"{index} x {mul} = {result}")
+    # f inside print is for print value in string 
+
+
+#write program that finds the sum of all numbers from 1 to 100 (hint: you will need a variable to keep track of the total while looping).the result above is 5050
+
+total = 0 
+for index  in range(1,101):
+    total += index
+print(f"The sum is {total}")
+
+#num1 = int("start: ")
+#num2 = int("end: ")
