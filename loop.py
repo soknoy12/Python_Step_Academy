19a994ca47d590b7da4daa93cb085a4c91511201
srey khuoch <khuochsrey35@gmail.com>
2025-09-16 21:59:13 +0700

    Add this as practice loop

---
+++ loop.py
@@ -0,0 +1,11 @@
+#lang = 'Calculater'
+#for character in lang:
+    #print(character)
+
+#for idx in range (0, len(lang)):
+#    print(lang[idx])
+
+#write the program that prints the numbers 1 to 10 
+ # Using a for loop
+for i in range(1, 11):
+    print(i+1)
