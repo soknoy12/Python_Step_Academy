bf50c65675810e0bd5e6a47580a3fd004e39d97e
PanhaSeth <theheroseth123456@gmail.com>
2025-10-04 20:12:08 -0700

    exerciese today

---
+++ ex.py
@@ -0,0 +1,6 @@
+for n in range (100, 1, -1):
+    if n % 7 == 0:
+        print("Boom")
+        continue
+    else:
+        print(n)
--- loops.py
+++ loops.py
@@ -1,3 +1,4 @@
+import random
 #blank///
 #! This is about ALL LOOPS in Python.
 #? For in Range() function loop. it iterate over fix number of times.
@@ -34,21 +35,76 @@
 
 #! Write a program, takes two numbers and operators (+,-,*,/)
 
-print("This is a calculator Program")
-z = (input("Choose the operators (+,-,*,/): "))
-x = int(input("Choose First Number: "))
-y = int(input("Choose Second Number: "))
+# print("This is a calculator Program")
+# z = (input("Choose the operators (+,-,*,/): "))
+# x = int(input("Choose First Number: "))
+# y = int(input("Choose Second Number: "))
 
-match z:
-    case '+':
-        result = x + y
-        print(result)
-    case '-':
-        result = x - y
-        print(result)
-    case '*':
-        result = x * y
-        print(result)
-    case '/':
-        result = x / y
-        print(result)
+# match z:
+#     case '+':
+#         result = x + y
+#         print(result)
+#     case '-':
+#         result = x - y
+#         print(result)
+#     case '*':
+#         result = x * y
+#         print(result)
+#     case '/':
+#         result = x / y
+#         print(result)
+
+
+#Print numbers until the user enters: 0
+# number = int(input("Enter a number: "))
+# while number != 0:
+#     print(f"You have enter {number}")
+#     number = int(input("Enter a number: "))
+
+# print("End of code")
+
+# number1 = int(input("Enter n: "))
+# for number2 in range(number1, 0, -1):
+#     print(number2)
+
+# total = 0
+
+# while (True):
+#     num = int(input("Enter number: "))
+#     if num == 0:
+#         break
+#     else:
+#         total += num
+# print(total)
+    
+
+
+# attempt = 0
+# while (True):
+#     guess = int(input("Guess number 1 to 100: "))
+#     attempt += 1
+#     correct = 69
+
+#     if guess == correct:
+#         print(f"Correct! you have try {attempt} times") 
+#         break
+#     elif guess < 69:
+#         print("Too low")
+#     else:
+#         guess >= 99
+#         print("Too high")
+    
+#different approach
+
+number = random.randint(1,100)
+guess = int(input("Enter number: "))
+count = 1
+
+while guess != number:
+    if guess > number:
+        print("Higher")
+    else:
+        print("Lower")
+    guess = int(input("Guess the numbers: "))
+    count += 1
+print(f"Correct! You have tried {count} times")
